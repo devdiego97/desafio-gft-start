dc64c30771c2fff4224e4661a794c96750ae462b
Diego Morais <diego97.git@gmail.com>
2025-08-21 10:43:07 -0300

    desagio 1 concluido

---
+++ cifra_do_estagiario/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+
+string texto = "";
+Console.WriteLine("Digite um texto:");
+         texto = Console.ReadLine();
+
+        var alteracoes = new Dictionary<char, char>
+        {
+            { 'a', '@' },
+            { 'A', '@' },
+            { 'e', '#' },
+            { 'E', '#' },
+            { 'i', '!' },
+            { 'I', '!' },
+            { 'o', '$' },
+            { 'O', '$' },
+            { 'u', '%' },
+            { 'U', '%' }
+        };
+
+        // StringBuilder é mais eficiente para manipular strings em loop
+        var sb = new StringBuilder();
+
+        foreach (char c in texto)
+        {
+            if (alteracoes.ContainsKey(c))
+                sb.Append(alteracoes[c]); // substitui
+            else
+                sb.Append(c); // mantém igual
+        }
+
+        Console.WriteLine("Texto alterado:");
+        Console.WriteLine(sb.ToString());
+    
